cc604d4f54c43284dbb57809f83f8702d55b3034
Hangil Kwon <54341018+navigator515@users.noreply.github.com>
2025-01-21 09:52:28 +0900

    initial commit

---
+++ pythonpro/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pythonpro.users.models import User
+from pythonpro.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
